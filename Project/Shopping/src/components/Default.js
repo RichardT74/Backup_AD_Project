7df5f75a733dc0dbbf8d18b93c5c0586c0ea0cbb
Richard Trist <richardtrist@richards-air-2.lan>
2020-01-26 21:53:59 +0000

    details.js altered

--- Project/Shopping/src/components/Default.js
+++ Project/Shopping/src/components/Default.js
@@ -11,29 +11,3 @@ export default class Default extends Component
     }
 }
 
-
-// import React, { Component } from "react";
-
-// export default class Default extends Component 
-// {
-// state = {
-//     loading: true,
-
-//   };
-
-//   async componentDidMount() {
-//     const url = "http://localhost:3001/storeProducts";
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     console.log(data);
-//     // this.setState({ storeProducts: data.results[0], loading: false });
-//   }
-
-//   render() {
-//       return  (
-//           <div>
-//               <h3>TestApi</h3>
-//           </div>
-//       )
-//     }
-// }
