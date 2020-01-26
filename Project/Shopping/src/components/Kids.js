2a9099e856f3d38679a51aebc9847f86df49220a
RichardT74 <richtrist@gmail.com>
2020-01-26 22:01:02 +0000

    Product list change

--- Project/Shopping/src/components/Kids.js
+++ Project/Shopping/src/components/Kids.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import Product from './Product';
 import Title from './Title';
-// import { storeProducts } from "../data"; 
+
 import {ProductConsumer} from '../context';  
 
 export default class Kids extends Component {
-    // state = {
-    //     products: storeProducts
-    // }; no longer needed after 
+
     render() {
       return ( 
            <React.Fragment>
